d630186b093a6d1ce4498ccacd3ba93e94f9b984
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-05-14 20:32:33 -0400

    Checkpoint 32 Complete - Associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
     def create
-      @post = Post.new(params.require(:post).permit(:title, :body))
+      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
       if @post.save #( == true/success )
         flash[:notice] = "Your post has been saved."
         redirect_to @post
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,26 @@
 require 'faker'
 
+#Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 #Create posts
 50.times do
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body:  Faker::Lorem.paragraph
   )
 end
-
 posts = Post.all
 
 #Create Comments
@@ -18,6 +31,15 @@ posts = Post.all
   )
 end
 
+#Add a user to log in with, for convenience.
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email:    'alex.kirsopp@gmail.com',
+  password: 'password'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
