bab4f435ce30da55701d4b230e9f2eb34bb2b9c2
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-05-30 15:23:40 -0400

    Checkpoint 40 (almost) complete - Comments. Need to fix comment.user.name in partial view.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:post_id])
+    @comment = current_user.comments.build(params.require(:comment).permit(:body))
+    @comment.post = @post
+    authorize @comment
+
+    if @comment.save
+      flash[:notice] = "Comment saved!"
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = @comment.errors.full_messages.to_sentence
+      redirect_to [@topic, @post]
+    end
+  end
+end
