a3961910bb1aadba9efcdbd6f02f18c68fc685f7
Alex Kirsopp <alex.kirsopp@gmail.com>
2015-06-11 19:59:47 -0400

    Popular posts, popular users feature complete. Optimized SQL queries for Topics list.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,15 @@ class User < ActiveRecord::Base
   def moderator?
     role == 'moderator'
   end
+
+  def self.top_rated
+    self.select('users.*') # Select all attribs of user
+        .select('COUNT(DISTINCT comments.id) AS comments_count') # Count comments by the user
+        .select('COUNT(DISTINCT posts.id) AS posts_count') # Count posts by the user
+        .select('COUNT(DISTINCT comments.id) + COUNT(DISTINCT posts.id) AS rank') # Add posts count and comments count and label the sum as 'rank'
+        .joins(:posts) # Ties posts table to users table via user_id
+        .joins(:comments) # Ties comments to users table via user_id
+        .group('users.id') # Group results, so each user will be returned in a distinct row
+        .order('rank DESC') # Order results by 'rank' created in this function
+  end
 end
